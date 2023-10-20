50841d5928eeb198bd4e14622fc08313883f2b87
Edwin Machaieie <machaieieedwin1@gmail.com>
2023-10-20 15:47:54 +0200

    Terminando o controller de usuario

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Contacto.Model;
+using Contacto.Repositorys.Interface;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Contacto.Controllers
@@ -7,5 +9,56 @@ namespace Contacto.Controllers
     [ApiController]
     public class UsuarioController : ControllerBase
     {
+        private readonly IUsuario _usuario;
+        public UsuarioController(IUsuario usuario) 
+        {
+            _usuario = usuario;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<UsuarioModelo>>> buscarTodos()
+        {
+            return Ok(await _usuario.Listar());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<UsuarioModelo>>> buscarComId(int id)
+        {
+            UsuarioModelo usuario = await _usuario.PegarComId(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+            return Ok(usuario);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<UsuarioModelo>>> Adicionar([FromBody] UsuarioModelo usuario)
+        {
+            if (usuario == null)
+            {
+                return BadRequest();
+            }
+            UsuarioModelo user = await _usuario.Adicionar(usuario);
+            return Ok(user);
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult<UsuarioModelo>> Atualizar([FromBody] UsuarioModelo usuario, int id)
+        {
+            UsuarioModelo usuarioModelo = await _usuario.Atualizar(usuario, id);
+            return Ok(usuarioModelo);
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Apagar(int id)
+        {
+            bool apagou = await _usuario.Remover(id);
+            if (!apagou)
+            {
+                return BadRequest();
+            }
+            return Ok();
+        }
+
     }
 }
